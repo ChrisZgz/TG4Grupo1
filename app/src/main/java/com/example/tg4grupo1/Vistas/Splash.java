946b0e4c6cb828bc25738a7760df28b749dcdcf4
Nicolás Del Amo <nico.delamo.perez@gmail.com>
2023-02-21 12:12:48 +0100

    Splash

--- app/src/main/java/com/example/tg4grupo1/Vistas/Splash.java
+++ app/src/main/java/com/example/tg4grupo1/Vistas/Splash.java
@@ -6,15 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.tg4grupo1.R;
 
 public class Splash extends AppCompatActivity {
+    Button iniciar;
+    EditText usuario;
+    EditText contrasena;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        iniciar = findViewById(R.id.bntInicioSesion);
+        usuario = findViewById(R.id.tNombre);
+        contrasena = findViewById(R.id.editTextTextPassword);
 
         Handler handler = new Handler(Looper.myLooper());
         handler.postDelayed(()->{
