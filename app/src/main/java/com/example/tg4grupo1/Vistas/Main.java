56ffad8913baaa2db648af3b3542dc3e6ac38aa4
Nicolás Del Amo <nico.delamo.perez@gmail.com>
2023-02-24 16:26:45 +0100

    URL MAIN

--- app/src/main/java/com/example/tg4grupo1/Vistas/Main.java
+++ app/src/main/java/com/example/tg4grupo1/Vistas/Main.java
@@ -6,15 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.text.method.LinkMovementMethod;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.tg4grupo1.R;
+
+import org.w3c.dom.Text;
 
 public class Main extends AppCompatActivity {
     Button iniciar;
     EditText usuario;
     EditText contrasena;
+    TextView codigo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,10 @@ public class Main extends AppCompatActivity {
         iniciar = findViewById(R.id.bntInicioSesion);
         usuario = findViewById(R.id.tNombre);
         contrasena = findViewById(R.id.editTextTextPassword);
+
+        codigo = findViewById(R.id.tLink);
+        codigo.setMovementMethod(LinkMovementMethod.getInstance());
+        
 
         iniciar.setOnClickListener(v->{
             if(!usuario.getText().toString().equals("usuario") ){
